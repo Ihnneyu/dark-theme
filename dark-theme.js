fed02c297503c1c5bd63f40e644622a87444ecb2
Ihnneyu <90972977+Ihnneyu@users.noreply.github.com>
2022-01-09 14:04:27 +0700

    Update dark-theme.js

--- dark-theme.js
+++ dark-theme.js
@@ -2,10 +2,12 @@
 // @name         Darkmode
 // @namespace    com.acbpro.darkmode
 // @version      2.3
-// @description  Blogtruyen helper
+// @description  dark-mode
 // @author       You
 // @match        https://www.thegioididong.com/*
 // @grant        GM_xmlhttpRequest
+// @connect  https://github.com/Ihnneyu/dark-theme/blob/main/dark-theme.min.css
+// @connect  self
 // @require http://code.jquery.com/jquery-3.3.1.min.js
 // @require https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.4/js/uikit.min.js
 // @require https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.4/js/components/notification.min.js
@@ -13,72 +15,36 @@
 // @require      https://unpkg.com/jszip@3.2.1/dist/jszip.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jszip-utils/0.0.2/jszip-utils.min.js
 // @require      https://unpkg.com/file-saver@2.0.1/dist/FileSaver.min.js
-// @updateURL   https://openuserjs.org/meta/trungking/Blocktruyen_Helper.meta.js
-// @downloadURL 
+// @updateURL   https://github.com/Ihnneyu/dark-theme/blob/main/dark-theme.js
+// @downloadURL https://github.com/Ihnneyu/dark-theme/blob/main/dark-theme.js
 // @license MIT
 // ==/UserScript==
 
 (function () {
   'use strict';
-  log('Blogtruyen Helper started');
   var Promise = window.Promise;
   if (!Promise) {
     Promise = JSZip.external.Promise;
   }
   const http = GM_xmlhttpRequest;
-  const id = '#bt-helper-offcanvas';
-  const body = 'body:not(#tinymce)';
-  const jsZip = new JSZip();
 
-  loadCSS('https://github.com/Ihnneyu/dark-theme/blob/main/drak-theme.css');
-  injectJS();
-  $(offcanvas).appendTo(body);
-
-
-  function urlToPromise(url, key) {
-    var urlObj = new URL(url),
-      urlPro = urlObj.protocol,
-      urlHost = urlObj.hostname,
-      urlRef = referer[urlHost] ? referer[urlHost] : urlHost,
-      urlOri = urlPro + '//' + urlRef;
-    return new Promise(function (resolve, reject) {
-      http({
-        method: 'GET',
-        url: url,
-        headers: {
-          referer: urlOri,
-          origin: urlOri
-        },
-        responseType: 'arraybuffer',
-        onload: function (response) {
-          if (response.response.byteLength < 10000 || (response.statusText !== 'OK' && response.statusText !== '')) {
-            log(response);
-            reject();
-          }
-          else {
-            downloaded++;
-            updateStatus();
-            resolve(response.response);
-          }
-        },
-        onerror: function (err) {
-          reject(err);
-        }
-      });
-    });
-  }
+  var base = window.document.createElement('base');
+  base.setAttribute('href', '/');
+  document.head.appendChild(base);
+  loadCSS('https://raw.githubusercontent.com/Ihnneyu/dark-theme/main/dark-theme.css');
+  document.body.setAttribute("class", "dark-theme");
 })();
-
-function toArray(obj) {
-  return $.map(obj, function (value, index) {
-    return [value];
-  });
-}
-
 function loadCSS(url) {
   var link = window.document.createElement('link');
   link.rel = 'stylesheet';
   link.type = 'text/css';
   link.href = url;
-  document.getElementsByTagName("HEAD")[0].appendChild(link);
+  var meta = window.document.createElement('meta');
+  meta.setAttribute('http-equiv', 'Content-Security-Policy');
+  var style = window.document.createElement('style');
+  fetch(url).then((r)=>{r.text().then((data)=>{ var css = data; var node = window.document.createTextNode(css);
+  style.appendChild(node);})});  
+  document.head.appendChild(meta);
+  document.head.appendChild(link);
+  document.head.appendChild(style);
 }
